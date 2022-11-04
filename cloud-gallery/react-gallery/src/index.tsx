c8e4eda1275d454dbc72d786ef4d4eb4632d06a7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-11-04 23:02:16 +0900

    misc

--- cloud-gallery/react-gallery/src/index.tsx
+++ cloud-gallery/react-gallery/src/index.tsx
@@ -5,35 +5,35 @@ const DELAY = 2000;
 let done = false;
 
 const Component: React.FC = () => {
-	if (done) {
-		done = false;
-		return (
-			<p>
-				<b>Hello World!!</b>
-			</p>
-		);
-	}
-	throw new Promise((resolve) =>
-		setTimeout(() => {
-			done = true;
-			resolve(true);
-		}, DELAY)
-	);
+  if (done) {
+    done = false;
+    return (
+      <p>
+        <b>Hello World!!</b>
+      </p>
+    );
+  }
+  throw new Promise((resolve) =>
+    setTimeout(() => {
+      done = true;
+      resolve(true);
+    }, DELAY)
+  );
 };
 
 export default {
-	async fetch(
-		request: Request,
-		env: Record<string, unknown>,
-		context: ExecutionContext
-	): Promise<Response> {
-		const stream = await renderToReadableStream(
-			<div>
-				<Suspense fallback={<p>Loading...</p>}>
-					<Component />
-				</Suspense>
-			</div>
-		);
-		return new Response(stream);
-	},
+  async fetch(
+    request: Request,
+    env: Record<string, unknown>,
+    context: ExecutionContext
+  ): Promise<Response> {
+    const stream = await renderToReadableStream(
+      <div>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Component />
+        </Suspense>
+      </div>
+    );
+    return new Response(stream);
+  },
 };
